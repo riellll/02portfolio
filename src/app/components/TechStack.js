b3438af9ab9f4d51de17d1ba372ffa71db63e7f3
unknown <griel034@gmail.com>
2023-07-21 20:39:23 +0800

    change

--- src/app/components/TechStack.js
+++ src/app/components/TechStack.js
@@ -21,18 +21,18 @@ const TechStack = () => {
         </h1>
       </div>
       <div className="grid grid-flow-col justify-center place-items-center gap-5 border-b-2 border-b-cyan-800 text-black">
-        <Image src={htmlLogo} alt="html" width={40} height={10} priority={false} className="w-full h-auto" />
-        <Image src={cssLogo} alt="css" width={60} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={javascriptlogo} alt="js" width={40} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={tailwindLogo} alt="tailwind" width={40} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={reactLogo} alt="react" width={50} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={nextjsLogo} alt="nextjs" width={60} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={expressLogo} alt="ex" width={60} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={mongodbLogo} alt="mongodb" width={50} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={nodejsLogo} alt="nodejs" width={50} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={phpLogo} alt="php" width={50} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={laravelLogo} alt="laravel" width={40} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
-        <Image src={mySQL} alt="mySQL" width={40} height={10} priority={false} style={{width: "100%", height: 'auto'}} />
+        <Image src={htmlLogo} alt="html" width={1000} height={1000} className="w-11 h-auto" priority/>
+        <Image src={cssLogo} alt="css" width={1000} height={1000} className="w-16 h-auto" priority/>
+        <Image src={javascriptlogo} alt="js" width={1000} height={500} className="w-12 h-auto" priority />
+        <Image src={tailwindLogo} alt="tailwind" width={1000} height={500} className="w-12 h-auto" priority />
+        <Image src={reactLogo} alt="react" width={1000} height={500} className="w-12 h-auto" priority />
+        <Image src={nextjsLogo} alt="nextjs" width={1000} height={1000} className="w-14 h-auto" priority />
+        <Image src={expressLogo} alt="ex" width={1000} height={1000} className="w-14 h-auto" priority />
+        <Image src={mongodbLogo} alt="mongodb" width={1000} height={1000} className="w-14 h-auto" priority />
+        <Image src={nodejsLogo} alt="nodejs" width={1000} height={1000} className="w-16 h-auto" priority />
+        <Image src={phpLogo} alt="php" width={1000} height={1000} className="w-16 h-auto" priority />
+        <Image src={laravelLogo} alt="laravel" width={1000} height={1000} className="w-16 h-auto" priority />
+        <Image src={mySQL} alt="mySQL" width={1000} height={1000} className="w-16 h-auto" priority />
       </div>
     </div>
   );
--- src/lib/GetMessage.js
+++ src/lib/GetMessage.js
@@ -2,12 +2,9 @@
 
 export default async function GetMessage() {
     const res = await fetch(`https://02portfolio.vercel.app/api/contact`,  { next: { revalidate: 10 } })
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-   
-    // Recommendation: handle errors
+ 
     if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
+
       throw new Error('Failed to fetch data')
     }
    
