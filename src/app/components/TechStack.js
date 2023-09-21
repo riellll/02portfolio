cbb6a21e0a69a4ecd3c38ee7bc359a4fd8f5eb68
unknown <griel034@gmail.com>
2023-09-21 22:45:36 +0800

    add change

--- src/app/components/TechStack.js
+++ src/app/components/TechStack.js
@@ -1,16 +1,6 @@
 import Image from "next/image";
-import htmlLogo from "@/tech_stack_pic/html.png"
-import cssLogo from "@/tech_stack_pic/css.png"
-import javascriptlogo from "@/tech_stack_pic/javascript.png"
-import tailwindLogo from "@/tech_stack_pic/tailwind.png"
-import reactLogo from "@/tech_stack_pic/react.png"
-import nextjsLogo from "@/tech_stack_pic/next-js-icon.svg"
-import expressLogo from "@/tech_stack_pic/express.png"
-import mongodbLogo from "@/tech_stack_pic/mongodb.png"
-import nodejsLogo from "@/tech_stack_pic/nodejs.png"
-import phpLogo from "@/tech_stack_pic/php.png"
-import laravelLogo from "@/tech_stack_pic/laravel.png"
-import mySQL from "@/tech_stack_pic/sql.png"
+import { techImage } from "@/constants";
+
 
 const TechStack = () => {
   return (
@@ -20,19 +10,10 @@ const TechStack = () => {
           Tech Stack
         </h1>
       </div>
-      <div className="flex flex-wrap justify-center place-items-center gap-5 border-b-2 border-b-gray-600 dark:border-b-cyan-800 text-black min-[320px]:px-6">
-        <Image src={htmlLogo} alt="html" width={1000} height={1000} className="w-11 h-auto" priority/>
-        <Image src={cssLogo} alt="css" width={1000} height={1000} className="w-16 h-auto" priority/>
-        <Image src={javascriptlogo} alt="js" width={1000} height={500} className="w-12 h-auto" priority />
-        <Image src={tailwindLogo} alt="tailwind" width={1000} height={500} className="w-12 h-auto" priority />
-        <Image src={reactLogo} alt="react" width={1000} height={500} className="w-12 h-auto" priority />
-        <Image src={nextjsLogo} alt="nextjs" width={500} height={500} className="w-10 h-auto" priority />
-        <Image src={expressLogo} alt="ex" width={1000} height={1000} className="w-16 h-auto" priority />
-        <Image src={mongodbLogo} alt="mongodb" width={1000} height={1000} className="w-14 h-auto" priority />
-        <Image src={nodejsLogo} alt="nodejs" width={1000} height={1000} className="w-16 h-auto" priority />
-        <Image src={phpLogo} alt="php" width={1000} height={1000} className="w-16 h-auto" priority />
-        <Image src={laravelLogo} alt="laravel" width={1000} height={1000} className="w-14 h-auto" priority />
-        <Image src={mySQL} alt="mySQL" width={1000} height={1000} className="w-14 h-auto" priority />
+      <div className="flex flex-wrap justify-center place-items-center gap-5 border-b-2 border-b-gray-600 dark:border-b-cyan-800 text-black min-[320px]:px-6 min-[320px]:pb-1">
+        {techImage.map(item => (
+          <Image src={item.imgURL} alt={item.alt} width={1000} height={1000} className={item.className} priority/>
+        ))}
       </div>
     </div>
   );
---
+++ src/constants/index.js
@@ -0,0 +1,89 @@
+import htmlLogo from "@/tech_stack_pic/html.png"
+import cssLogo from "@/tech_stack_pic/css.png"
+import javascriptlogo from "@/tech_stack_pic/javascript.png"
+import tailwindLogo from "@/tech_stack_pic/tailwind.png"
+import reactLogo from "@/tech_stack_pic/react.png"
+import nextjsLogo from "@/tech_stack_pic/next-js-icon.svg"
+import expressLogo from "@/tech_stack_pic/express.png"
+import mongodbLogo from "@/tech_stack_pic/mongodb.png"
+import nodejsLogo from "@/tech_stack_pic/nodejs.png"
+import phpLogo from "@/tech_stack_pic/php.png"
+import laravelLogo from "@/tech_stack_pic/laravel.png"
+import mySQL from "@/tech_stack_pic/sql.png"
+
+
+export const techImage = [
+  {
+    imgURL: htmlLogo,
+    alt: "html",
+    className: "w-11 h-auto",
+  },
+  {
+    imgURL: cssLogo,
+    alt: "css",
+    className: "w-16 h-auto",
+  },
+  {
+    imgURL: javascriptlogo,
+    alt: "js",
+    className: "w-12 h-auto",
+  },
+  {
+    imgURL: tailwindLogo,
+    alt: "tailwind",
+    className: "w-12 h-auto",
+  },
+  {
+    imgURL: reactLogo,
+    alt: "react",
+    className: "w-12 h-auto",
+  },
+  {
+    imgURL: nextjsLogo,
+    alt: "nextjs",
+    className: "w-10 h-auto",
+  },
+  {
+    imgURL: expressLogo,
+    alt: "ex",
+    className: "w-16 h-auto",
+  },
+  {
+    imgURL: mongodbLogo,
+    alt: "mongodb",
+    className: "w-14 h-auto",
+  },
+  {
+    imgURL: nodejsLogo,
+    alt: "nodejs",
+    className: "w-16 h-auto",
+  },
+  {
+    imgURL: phpLogo,
+    alt: "php",
+    className: "w-16 h-auto",
+  },
+  {
+    imgURL: laravelLogo,
+    alt: "laravel",
+    className: "w-14 h-auto",
+  },
+  {
+    imgURL: mySQL,
+    alt: "mySQL",
+    className: "w-14 h-auto",
+  },
+
+];
+  
+  export const profileTabs = [
+    { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+    { value: "replies", label: "Replies", icon: "/assets/members.svg" },
+    { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
+  ];
+  
+  export const communityTabs = [
+    { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+    { value: "members", label: "Members", icon: "/assets/members.svg" },
+    { value: "requests", label: "Requests", icon: "/assets/request.svg" },
+  ];
